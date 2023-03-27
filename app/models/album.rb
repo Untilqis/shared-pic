55381e57116463f83e31e2a66a811befdf58024d
Untilqis <haramakityuta2012@gmail.com>
2023-03-27 21:11:34 +0900

    画像投稿機能の準備

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album < ApplicationRecord
   #アソシエーション
   belongs_to :user
   #has_many :comments, dependent: :destroy
-  #has_one_attached :image
+  has_one_attached :image
   
   #バリデーション
   validates :title,      presence: true

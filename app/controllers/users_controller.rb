c4be6aef76d11f3303ce93ec06eedb6411e40d26
Untilqis <haramakityuta2012@gmail.com>
2023-05-23 15:13:31 +0900

    ユーザー編集機能(仮実装)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,33 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
   def show
-    @user = User.find(params[:id])
     @albums = @user.albums
   end
+
+  def edit
+    unless current_user == @user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      bypass_sign_in(@user)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "albums#index"
   get '/album/tag/:name', to: "albums#tag"
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
   resources :albums do
     resources :comments, only: :create
   end
